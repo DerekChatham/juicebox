f2c84546f27835e385282b8538b7a27567da7d77
Derek Chatham <Derek.ChathamSr@outlook.com>
2022-01-03 14:44:43 -0600

    Collecting Common Middleware

--- index.js
+++ index.js
@@ -2,7 +2,18 @@ const PORT = 3000;
 const express = require('express');
 const server = express();
 
+//Middleware
+server.use((req, res, next) => {
+    console.log("<____Body Logger START____>");
+    console.log(req.body);
+    console.log("<_____Body Logger END_____>");
+  
+    next();
+  });
 
+//Router
+const apiRouter = require('./api');
+server.use('/api', apiRouter);
 
 server.listen(PORT, () => {
   console.log('The server is up on port', PORT)
